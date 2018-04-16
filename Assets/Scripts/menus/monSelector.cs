897047ab451c2551a480e41ddf871612aa0c8b8b
Unknown <adamtubauvilar@enti.cat>
2018-04-16 19:08:34 +0200

    funcionament endpanel canvis nivells i buttons levelselector

--- Assets/Scripts/menus/monSelector.cs
+++ Assets/Scripts/menus/monSelector.cs
@@ -6,34 +6,6 @@ using UnityEngine.UI;
 
 public class monSelector : MonoBehaviour {
 
-	public int level;
-	public Button[] levelButtons;
-
-	void Start () 
-	{      
-        int levelReached = PlayerPrefs.GetInt ("levelReached");
-
-        for (int i = 0; i < levelButtons.Length; i++) {
-			if (i + 1 > level)//levelReached)
-				levelButtons [i].interactable = false;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
-	}
-
-}
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-
-public class monSelector : MonoBehaviour {
-
 	//public int level;
 	public Button[] levelButtons;
 
